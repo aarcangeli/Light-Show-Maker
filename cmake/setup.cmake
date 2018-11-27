068b24714318a6f77ae13b268a93ddc2c7c1bfa0
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2018-11-27 21:02:15 +0100

    thread check

--- cmake/setup.cmake
+++ cmake/setup.cmake
@@ -1,3 +1,5 @@
+include(CheckCXXSourceRuns)
+
 set(CMAKE_CXX_STANDARD 11)
 set(ROOT_DIRECTORY ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_SOURCE_DIR}")
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${ROOT_DIRECTORY}/cmake")
@@ -10,4 +12,21 @@ endif ()
 
 if (MSVC)
     set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE} /ENTRY:mainCRTStartup")
+endif ()
+
+CHECK_CXX_SOURCE_RUNS("
+    #include <thread>
+    int returnValue = 1;
+    void work() {
+        returnValue = 0;
+    }
+    int main() {
+        std::thread test(&work);
+        test.join();
+        return returnValue;
+    }
+" WITH_THREAD)
+
+if (NOT WITH_THREAD)
+    message(FATAL_ERROR "This compiler doesn't support C++11 std::thread")
 endif ()
