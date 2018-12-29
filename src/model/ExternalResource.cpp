b140ba4d718d167dab9e60993e4145e310ed2ebf
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2018-12-30 00:34:19 +0100

    fix appveyor.yml

--- src/model/ExternalResource.cpp
+++ src/model/ExternalResource.cpp
@@ -1,1 +1,5 @@
 #include "ExternalResource.h"
+
+std::vector<uint8_t> sm::model::ExternalResource::load() {
+    return {};
+}
--- src/model/ExternalResource.h
+++ src/model/ExternalResource.h
@@ -1,12 +1,19 @@
 #ifndef EXTERNALRESOURCE_H
 #define EXTERNALRESOURCE_H
 
+#include "string"
+#include "vector"
 
 namespace sm {
 namespace model {
 
+// a reference to an external file (an image, sound ecc...)
+// a resource will never be saved by LSM
 class ExternalResource {
+public:
+    std::string absoluteFilename;
 
+    std::vector<uint8_t> load();
 };
 
 }
