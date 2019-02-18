6987a8f95d970962763764cf638c6b15aa044abd
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2019-02-19 00:40:56 +0100

    fix

--- src/lightshowmaker/main.cpp
+++ src/lightshowmaker/main.cpp
@@ -7,11 +7,6 @@ using namespace sm;
 static void glfw_error_callback(int error, const char *description);
 
 int main(int argc, char **argv) {
-    float source = 1e20f; // a number logically large for a 16 bit signed integer
-    int32_t converted = (int32_t) source;
-    printf("converted: %i", converted);
-    return 0;
-
     glfwSetErrorCallback(glfw_error_callback);
     setupDpi();
 
